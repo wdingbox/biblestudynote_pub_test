03d6a7eb65c29bca1e62d279a83d95ca827f8290
System Administrator <wdingbox@gmail.com>
2020-12-31 13:12:12 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved.. 2020-12-31,12:40:16 PM.

---
+++ account/dat/localStorage_json.js
@@ -0,0 +1,9 @@
+var localStorage = {
+    "#RecentMarks": "[\"Gen24:1\"]",
+    "FontSize": "16",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"2020-12-31,12:40:16 PM. \"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\",\"repodesc\":\"2020-12-31,12:40:16 PM. \"}]",
+    "#MemoryVerse": "[\"Gen24:1\"]",
+    "test": "1,2",
+    "BookNameLanguage": "English",
+    "#RecentBooks": "[\"1Pe1:1\",\"Gen1:1\"]"
+}
