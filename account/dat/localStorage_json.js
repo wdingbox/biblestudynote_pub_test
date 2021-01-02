eb142f12bc9042a4fac57cc91fe3050f9217b082
System Administrator <wdingbox@gmail.com>
2021-01-01 19:01:54 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,9 +1,7 @@
 var localStorage = {
-    "#RecentMarks": "[\"Gen24:1\"]",
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"2020-12-31,12:40:16 PM. \"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\",\"repodesc\":\"2020-12-31,12:40:16 PM. \"}]",
-    "#MemoryVerse": "[\"Gen24:1\"]",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\"}]",
+    "#MemoryVerse": "[\"Gen24:1\",\"Pro16:9\"]",
     "test": "1,2",
-    "BookNameLanguage": "English",
-    "#RecentBooks": "[\"1Pe1:1\",\"Gen1:1\"]"
+    "#RecentMarks": "[\"Gen24:1\"]"
 }
