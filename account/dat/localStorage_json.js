14d1785c87ded497e36e457058bf301d06b3985f
System Administrator <wdingbox@gmail.com>
2021-01-02 21:00:42 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,6 +1,6 @@
 var localStorage = {
     "FontSize": "16",
     "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
-    "#MemoryVerse": "[\"Pro16:9\",\"Gen24:1\",\"Jhn3:16\"]",
+    "#MemoryVerse": "[\"Jhn3:16\",\"Gen24:1\",\"Pro16:9\"]",
     "test": "1,2"
 }
