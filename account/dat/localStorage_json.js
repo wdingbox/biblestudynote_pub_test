619003c450503aa9bba4fc1e6edecbb55596482c
System Administrator <wdingbox@gmail.com>
2021-01-02 22:19:07 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,5 +1,5 @@
 var localStorage = {
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSAxx\",\"repodesc\":\"yuy\"}]",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSAxx\",\"repodesc\":\"\"}]",
     "test": "1,2"
 }
