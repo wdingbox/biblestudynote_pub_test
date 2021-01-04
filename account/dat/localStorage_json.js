30a0f3bbb11cb10f255529b8f027c4eb9b6869fc
System Administrator <wdingbox@gmail.com>
2021-01-04 04:06:21 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,5 +1,8 @@
 var localStorage = {
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"2021-01-04,3:55:42 AM. \"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxy.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxxxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"}]",
-    "test": "1,2"
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxy.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxxxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"}]",
+    "#MemoryVerse": "[\"Pro8:22\",\"Pro16:9\"]",
+    "test": "1,2",
+    "#RecentMarks": "[\"Pro8:22\",\"Pro16:9\"]",
+    "#RecentBooks": "[\"Pro1:1\"]"
 }
