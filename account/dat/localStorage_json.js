2e56fce98706d1f43e4cc698ee4d66918bd1bf81
System Administrator <wdingbox@gmail.com>
2021-01-06 16:03:21 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,8 +1,9 @@
 var localStorage = {
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxy.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxxxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"}]",
-    "#MemoryVerse": "[\"Pro8:22\",\"Pro16:9\"]",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
+    "myNote": "",
     "test": "1,2",
-    "#RecentMarks": "[\"Pro8:22\",\"Pro16:9\"]",
-    "#RecentBooks": "[\"Pro1:1\"]"
+    "#RecentMarks": "[\"Col1:15\",\"Pro16:9\"]",
+    "#RecentBooks": "[\"Pro1:1\",\"Col1:1\"]",
+    "#MemoryVerse": "[\"Col1:15\",\"Pro16:9\"]"
 }
