007db196201ba1e88bc46d7d0ad1e305ed5ee1c2
root <root@ip-172-31-59-85.ec2.internal>
2021-01-07 22:43:51 +0000

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_test/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,9 +1,12 @@
 var localStorage = {
-    "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
-    "myNote": "",
+    "#RecentMarks": "[\"Eph6:12\",\"1Ts2:2\",\"Phl1:30\",\"2Co10:3\",\"Eph2:2\",\"1Ts4:17\",\"1Co9:26\",\"Col1:14\",\"Col1:15\",\"Gen24:17\"]",
+    "MostRecentSearchFile": "NIV",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA \",\"repodesc\":\"\"}]",
+    "FontSize": "20",
+    "myNote": "{\"host\":\"52.87.154.78:7778\",\"url\":\"http://52.87.154.78:7778/\",\"api\":\"ApiBibleObj_write_Usr_BkcChpVrs_txt\",\"inp\":{\"usr\":{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},\"par\":{\"fnames\":[\"_myNote\"],\"inpObj\":{\"Gen\":{\"24\":{\"17\":\"<ol><li>\\\"有一个撒玛利亚的妇人来打水．耶稣对他说、请你给我水喝。\\\" (Jhn4:7 CUVS)</li></ol>\"}}}}},\"m_src\":\"http://52.87.154.78:7778/ApiBibleObj_read_Usr_BkcChpVrs_txt?inp=%7B%22usr%22%3A%7B%22repopath%22%3A%22https%3A%2F%2Fgithub.com%2Fwdingbox%2Fbiblestudynote_pub_wd.git%22%2C%22passcode%22%3A%223edcFDSA%22%2C%22repodesc%22%3A%22%22%7D%2C%22par%22%3A%7B%22fnames%22%3A%5B%22_myNote%22%5D%2C%22inpObj%22%3A%7B%22Gen%22%3A%7B%2224%22%3A%7B%2217%22%3A%22%22%7D%7D%7D%7D%7D\"}",
+    "MostRecentSearchStrn": "[\"dragon\",\"tu zi\",\"zheng zhan\",\"kong zhong\",\"kong qi\",\"dou\"]",
+    "#RecentBooks": "[\"Gen1:1\",\"Col1:1\"]",
     "test": "1,2",
-    "#RecentMarks": "[\"Col1:15\",\"Pro16:9\"]",
-    "#RecentBooks": "[\"Pro1:1\",\"Col1:1\"]",
-    "#MemoryVerse": "[\"Col1:15\",\"Pro16:9\"]"
+    "SelectedDocsList": "NIV,CUVpy,_myNote,CUVS,ESV",
+    "#MemoryVerse": "[\"Pro16:9\",\"Col1:15\",\"1Ts2:2\",\"Phl1:30\",\"Eph6:12\",\"2Co10:3\",\"Eph2:2\",\"1Ts4:17\",\"1Co9:26\",\"Pro8:22\"]"
 }
